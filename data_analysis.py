d61fa68dd61b164e928c1456fc8347085774450f
erick <erick.iv.lopz@hotmail.com>
2025-07-09 15:29:54 -0700

    adding features:spends, avg and top by category

--- data_analysis.py
+++ data_analysis.py
@@ -1,22 +1,23 @@
+import pandas as pd
 class DataAnalysis:
     def __init__(self, data):
         self.data = data
-
+        self.spending_by_category = self.data[self.data['Type'] == 'Expense'].groupby('Category')['Amount'].sum()
     def analyze_spending_by_category(self):
         print("TODO: Analyzing spending by category")
-        spending_by_category = data[data['Type'] == 'Expense'].groupby('Category')['Amount'].sum()
-        print(spending_by_category)
+        print(self.spending_by_category)
 
     def calculate_average_monthly_spending(self):
         print("TODO: Calculating average monthly spending")
-        data['Date'] = pd.to_datetime(data['Date'])
-        monthly_spending = data[data['Type'] == 'Expense'].groupby(data['Date'].dt.month_name())['Amount'].mean()
+        self.data['Date'] = pd.to_datetime(self.data['Date'])
+        monthly_spending = self.data[self.data['Type'] == 'Expense'].groupby(self.data['Date'].dt.month_name())['Amount'].mean()
         print(round(monthly_spending, 2))
 
     def show_top_spending_category(self):
         print("TODO: Showing top spending category")
-        category_max = spending_by_category.idxmax()
-        amount_max = spending_by_category.max()
+
+        category_max = self.spending_by_category.idxmax()
+        amount_max = self.spending_by_category.max()
         print(f'{category_max} with ${amount_max} total spending is the highest.')
     
     def visualize_monthly_spending_trend(self):
