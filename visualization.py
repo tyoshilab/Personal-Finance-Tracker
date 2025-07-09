89a6678174fa29a8a677e0c9264c3389756925fa
tyoshilab <aiko.jitsui@gmail.com>
2025-07-08 18:38:39 -0700

    add: basic visualization logic

--- visualization.py
+++ visualization.py
@@ -4,14 +4,36 @@ class DataVisualization:
     def __init__(self, data):
         self.data = data
 
-    def visualize_monthly_spending_trend(self):
-        print("TODO: Visualizing monthly spending trend")
+    def monthly_spending_trend(self):
+        monthly_data = self.data[['Date', 'Amount']].groupby(self.data['Date'].dt.to_period('M')).sum('Amount')
+        plt.plot(monthly_data.index.astype(str), monthly_data['Amount'], marker='o')
+        plt.title('Monthly Spending Trend')
+        plt.xlabel('Month')
+        plt.ylabel('Total Spending')
+        print("Visualizing monthly spending trend...")
+        plt.show()
 
-    def visualize_monthly_income_vs_spending(self):
+    def spending_by_category(self):
+        category_data = self.data.groupby('Category')['Amount'].sum().sort_values(ascending=False)
+        plt.bar(category_data.index, category_data.values)
+        plt.title('Spending by Category')
+        plt.xlabel('Category')
+        plt.ylabel('Total Spending')
+        print("Visualizing spending by category...")
+        plt.show()
+    
+    def percentage_categories(self):
+        category_data = self.data.groupby('Category')['Amount'].sum()
+        plt.pie(category_data, labels=category_data.index, autopct='%1.1f%%', startangle=140)
+        plt.title('Spending Distribution by Category')
+        print("Visualizing spending distribution by category...")
+        plt.show()
+
+    def monthly_income_vs_spending(self):
         print("ex_TODO: Visualizing monthly income vs spending")
 
-    def visualize_category_spending_vs_budget(self):
+    def category_spending_vs_budget(self):
         print("ex_TODO: Visualizing spending distribution")
 
-    def visualize_income_and_expenses(self):
+    def income_and_expenses(self):
         print("ex_TODO: Visualizing income and expenses")
